abb6d81737fe06b3e1857507e2f6b973ff9fd5b5
Abhisar Prince Jha <abhisarprincejha@gmail.com>
2026-05-14 01:38:41 +0530

    refactored to tanstack and added button to rate

--- actions/Apicall.ts
+++ actions/Apicall.ts
@@ -1,21 +1,6 @@
-import Pokedex from 'pokedex-promise-v2';
-const P = new Pokedex();
-export async function PokemonData(name : string){
-    
-}
 
-export async function AllPokeData(){
+// export async function PokemonData(name : string){
     
-    let data;
-    data = await P.getPokemonsList({limit : 200 , offset:0});
-    let detailedData = await Promise.all(
-        data.results.map(async (curr : any) => {
-            return await P.getPokemonByName(curr.name);
-        })
-    )
-    console.log(detailedData);
-    return detailedData;
-};
-
+// }
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,39 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
 import pokelogo from "../public/pokelogo.jpg";
 import ClipLoader from "react-spinners/ClipLoader";
-import { AllPokeData } from "@/actions/Apicall";
+import { APIResourceList, NamedAPIResource, Pokemon } from "pokedex-promise-v2";
 import { Fredoka } from "next/font/google";
-
+import { useQuery } from "@tanstack/react-query";
 const fredoka = Fredoka({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
 });
+import Pokedex from "pokedex-promise-v2";
+import { id } from "zod/locales";
+
+async function AllPokeData() {
+  const P = new Pokedex();
+  let data;
+  data = await P.getPokemonsList({ limit: 20, offset: 0 });
+  let detailedData = await Promise.all(
+    data.results.map(async (curr: NamedAPIResource) => {
+      return await P.getPokemonByName(curr.name);
+    }),
+  );
+  console.log(detailedData);
+  return detailedData;
+}
 
 const page = () => {
-  const [pokemon, setpokemon] = useState<any[]>([]);
-  const [isloading, setisloading] = useState(true);
+  let { isLoading, isError, data, error } = useQuery<Pokemon[]>({
+    queryKey: ["pokemon"],
+    queryFn: AllPokeData,
+    staleTime: 1000 * 60 * 10,
+  });
 
-  useEffect(() => {
-    // async function fetchData() {
-    //   setisloading(true);
-    //   const data = await (
-    //     await fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
-    //   ).json();
-    //   setpokemon(data);
-    //   setisloading(false);
-    // }
-    AllPokeData().then((res) => {
-      setpokemon(res);
-      setisloading(false);
-    });
-  }, []);
+  if (isError || error) return <div>"Something Went Wrong"</div>;
+
   return (
     <div className="grid grid-rows-[1fr_5fr_1fr] h-screen gap-4">
       <div className="border border-zinc-500 rounded-3xl text-white flex justify-center align-middle gap-4 font-bold text-8xl p-4">
@@ -37,7 +42,7 @@ const page = () => {
       </div>
 
       <div className="text-white p-7 rounded-3xl">
-        {isloading ? (
+        {isLoading ? (
           <div className="flex justify-center items-center h-screen">
             <ClipLoader size={50} color="#f1f1ff" speedMultiplier={1} />
           </div>
@@ -53,7 +58,10 @@ const page = () => {
         gap-9
       "
           >
-            {pokemon.map((curr) => {
+            {data?.map((curr: any) => {
+              let hex = Math.floor(Math.random() * 0xffffffff)
+                .toString(16)
+                .padEnd(8, "0");
               return (
                 <div
                   key={curr.name}
@@ -89,25 +97,51 @@ const page = () => {
                         Abilities :
                       </p>
 
-                      {curr.abilities.map((data) => {
+                      {curr.abilities.map((data: APIResourceList) => {
                         let current = data.ability.name;
                         return (
-                          <>
+                          <div key={current}>
                             <div
                               key={current}
                               className="flex justify-center capitalize text-xl font-light px-2 border-l-white"
                             >
-                              {current} ,
+                              {current}
                             </div>
-                          </>
+                          </div>
                         );
                       })}
                     </div>
                     {/* Types */}
-                    <div className="p-2 text-xl font-bold m-2 ">
-                      <p>Type : </p>
-                      {}
-                    </div>
+                    {
+                      <div
+                        key={curr.id}
+                        className="flex justify-start p-2 text-xl font-light m-2 capitalize gap-2 "
+                      >
+                        <p>
+                          Type : {curr.types[0].type.name} ,{" "}
+                          {curr.types[1]?.type.name} {curr.types[2]?.type.name}
+                        </p>
+                      </div>
+                    }
+
+                    <button
+                      style={{ backgroundColor: `#${hex}` }}
+                      className="
+    rounded-2xl
+    p-4
+    flex
+    justify-center
+    items-center
+    text-4xl
+    text-white
+    hover:scale-125
+    cursor-pointer
+    transition
+  "
+                    
+                    >
+                      Rate this Pokemon
+                    </button>
                   </div>
                 </div>
               );
